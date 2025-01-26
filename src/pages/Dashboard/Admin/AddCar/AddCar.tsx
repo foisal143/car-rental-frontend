e1f082e311e34e1408dd43739b51767cf05e8271
Md Foisal Islam <islammdfoisal54@gmail.com>
2025-01-26 23:37:32 +0600

    add car form added

--- src/pages/Dashboard/Admin/AddCar/AddCar.tsx
+++ src/pages/Dashboard/Admin/AddCar/AddCar.tsx
@@ -1,5 +1,246 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type CarFormData = {
+  name: string;
+  image: string;
+  description: string;
+  color: string;
+  isElectric: boolean;
+  status?: 'available' | 'unavailable';
+  features: string[];
+  pricePerHour: number;
+  isDeleted?: boolean;
+};
 const AddCar = () => {
-  return <div>this is add car page</div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm<CarFormData>();
+  const [uploading, setUploading] = useState(false);
+  const [isElectric, setIsElectric] = useState(true);
+  const onSubmit: SubmitHandler<CarFormData> = async data => {
+    console.log(data);
+    reset();
+  };
+
+  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    setUploading(true);
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+      const response = await axios.post(
+        `https://api.imgbb.com/1/upload?key=YOUR_IMGBB_API_KEY`,
+        formData
+      );
+      const imageUrl = response.data.data.url;
+      setValue('image', imageUrl); // Set the image URL in the form
+    } catch (error) {
+      console.error('Image upload failed:', error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return (
+    <div className="p-8">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="max-w-2xl  mx-auto space-y-6 p-4 font-text rounded-md "
+      >
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Name Field */}
+          <div>
+            <label htmlFor="name" className="block font-medium">
+              Car Name
+            </label>
+            <input
+              placeholder="Enter your name"
+              id="name"
+              {...register('name', { required: 'Car name is required' })}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name.message}</p>
+            )}
+          </div>
+
+          {/* Image Upload */}
+          <div>
+            <label htmlFor="image" className="block font-medium">
+              Upload Image
+            </label>
+            <input
+              id="image"
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+            {uploading && <p className="text-blue-500 text-sm">Uploading...</p>}
+            {errors.image && (
+              <p className="text-red-500 text-sm">{errors.image.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Description Field */}
+          <div>
+            <label htmlFor="description" className="block font-medium">
+              Description
+            </label>
+            <textarea
+              placeholder="Enter car description"
+              id="description"
+              {...register('description', {
+                required: 'Description is required',
+              })}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+            {errors.description && (
+              <p className="text-red-500 text-sm">
+                {errors.description.message}
+              </p>
+            )}
+          </div>
+
+          {/* Color Field */}
+          <div>
+            <label htmlFor="color" className="block font-medium">
+              Color
+            </label>
+            <input
+              placeholder="Enter car color"
+              id="color"
+              {...register('color', { required: 'Color is required' })}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            />
+            {errors.color && (
+              <p className="text-red-500 text-sm">{errors.color.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Is Electric Field with Stylish Radio Buttons */}
+          <div>
+            <label className="block font-medium">Electric</label>
+            <div className="flex space-x-4 mt-2">
+              {/* Yes Option */}
+              <label className="flex items-center cursor-pointer space-x-2 px-3 py-2 rounded-md border border-gray-300 hover:bg-blue-50 transition">
+                <input
+                  type="radio"
+                  value="true" // Set the value for "Yes"
+                  {...register('isElectric', {
+                    required: 'Please select if the car is electric',
+                  })}
+                  className=""
+                />
+                <div className="w-4 h-4 border-2 border-[#ff4c31] rounded-full flex items-center justify-center">
+                  {isElectric && (
+                    <div className="w-2 h-2 bg-primary rounded-full"></div>
+                  )}
+                </div>
+                <span>Yes</span>
+              </label>
+
+              {/* No Option */}
+              <label className="flex items-center cursor-pointer space-x-2 px-3 py-2 rounded-md border border-gray-300 hover:bg-blue-50 transition">
+                <input
+                  type="radio"
+                  value="false" // Set the value for "No"
+                  {...register('isElectric', {
+                    required: 'Please select if the car is electric',
+                  })}
+                  className=""
+                />
+                <div className="w-4 h-4 border-2 border-[#ff4c31] rounded-full flex items-center justify-center">
+                  {isElectric === false && (
+                    <div className="w-2 h-2 bg-primary rounded-full"></div>
+                  )}
+                </div>
+                <span>No</span>
+              </label>
+            </div>
+            {errors.isElectric && (
+              <p className="text-red-500 text-sm">
+                {errors.isElectric.message}
+              </p>
+            )}
+          </div>
+
+          {/* Status Field */}
+          <div>
+            <label htmlFor="status" className="block font-medium">
+              Status
+            </label>
+            <select
+              id="status"
+              {...register('status')}
+              className="border border-gray-300 rounded px-3 py-2 w-full"
+            >
+              <option value="available">Available</option>
+              <option value="unavailable">Unavailable</option>
+            </select>
+          </div>
+        </div>
+
+        <div>
+          {/* Features Field */}
+          <label htmlFor="features" className="block font-medium">
+            Features (comma-separated)
+          </label>
+          <input
+            placeholder="Enter car features "
+            id="features"
+            {...register('features', {
+              setValueAs: value =>
+                value.split(',').map((feature: string) => feature.trim()),
+            })}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
+          />
+        </div>
+
+        <div>
+          {/* Price Per Hour Field */}
+          <label htmlFor="pricePerHour" className="block font-medium">
+            Price Per Hour ($)
+          </label>
+          <input
+            id="pricePerHour"
+            placeholder="Enter car price"
+            type="number"
+            {...register('pricePerHour', {
+              required: 'Price per hour is required',
+              min: 1,
+            })}
+            className="border border-gray-300 rounded px-3 py-2 w-full"
+          />
+          {errors.pricePerHour && (
+            <p className="text-red-500 text-sm">
+              {errors.pricePerHour.message}
+            </p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="btn-primary w-full py-2 rounded text-white"
+        >
+          Add Car
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddCar;
